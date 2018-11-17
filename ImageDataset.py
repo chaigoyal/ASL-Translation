fff1a98a9ff83260bba78f52c185b7df532a9401
rahuldesai1 <rahuldesai@berkeley.edu>
2018-11-17 14:03:21 -0800

    Adding the InceptionV3 transfer model along with other files

--- ImageDataset.py
+++ ImageDataset.py
@@ -46,46 +46,7 @@ class ImageDataset():
 	def augment_data(self):
 
 
-def add_files_to_directory(num_files):
-	
-	count = 1
-	index = 0
-	indices = []
 
-	aug_data_dir = '/Users/rahuldesai/LocalDocs/Projects/ASL-Translation/ASL-Datasets/aug_dataset'
-	directory = '/Users/rahuldesai/LocalDocs/Projects/ASL-Translation/ASL-Datasets/asl_alphabet_train'
-	
-	for folder in listdir(directory):
-		print(folder)
-		if folder not in listdir(aug_data_dir):
-			os.mkdir(aug_data_dir + '/' + folder)
-		else:
-			sorted_list = listdir(aug_data_dir + '/' + folder)
-			sorted_list.sort()
-			last = sorted_list[-1]
-			last_value = int(last[1:5])
-		
-		folder_dir = directory + '/' + folder
-		filenames = listdir(folder_dir)
-		
-		while count <= num_files:
-			index = random.randint(0, len(filenames) - 1)
-			
-			if index not in indices:
-				indices.append(index)
-
-				file_name = folder + str(last_value + count) + '_train.jpg'
-				
-				old_path = folder_dir + '/' + filenames[index]
-				new_path = aug_data_dir + '/' + folder + '/' + file_name
-
-				shutil.copy(old_path, new_path)
-				count += 1
-		
-		count = 1
-		indices = []
-	
-	print('Done.')
 
 			
 
